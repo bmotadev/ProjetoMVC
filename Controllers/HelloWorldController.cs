feff499601d5e224b013752c9617af1b520641da
Bruno Mota <bmotadev@gmail.com>
2023-06-05 23:08:19 -0300

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ProjetoMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        //public string Index()
+        {
+            return View();
+            //return "Apresenta esta mensagem";
+        }
+
+        public IActionResult Welcome(string nome, int numTimes = 10)
+        {
+            //return HtmlEncoder.Default.Encode($"Oi {nome}, ID: {ID}");
+            //return "Estou no metodo Welcome do HelloWorldController";
+
+            ViewData["Message"] = "Hello " + nome;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
